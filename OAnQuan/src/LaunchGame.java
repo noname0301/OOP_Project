f4220010238957ee00849962a351caf706bef591
noname0301 <106442939+noname0301@users.noreply.github.com>
2024-12-26 07:28:41 +0700

    update

--- OAnQuan/src/LaunchGame.java
+++ OAnQuan/src/LaunchGame.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class LaunchGame extends Application {
@@ -27,6 +28,8 @@ public class LaunchGame extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.setTitle("O an quan");
         primaryStage.show();
+        Image icon = new Image("/icon.png"); 
+        primaryStage.getIcons().add(icon);
     }
 
     public static void main(String[] args) {
